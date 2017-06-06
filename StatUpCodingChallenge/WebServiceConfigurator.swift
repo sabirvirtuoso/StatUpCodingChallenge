0c3b700b701fd91d94d49863d01d05268da9034c
Syed Sabir Salman-Al-Musawi <sabirvirtuoso@gmail.com>
2017-06-06 19:35:58 +0600

    Add WebServiceHelper and its Configurator

---
+++ StatUpCodingChallenge/WebServiceConfigurator.swift
@@ -0,0 +1,24 @@
+//
+//  WebServiceConfigurator.swift
+//  StatUpCodingChallenge
+//
+//  Created by Syed Sabir Salman on 6/6/17.
+//  Copyright © 2017 Syed Sabir Salman. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+open class WebServiceConfigurator {
+    
+    typealias T = (url: String, mapper: (Any?) -> (Any?))
+    
+    static let wordOfTheDayPath = Bundle.main.infoDictionary!["wordOfTheDayApi"] as? String
+    
+    static let awsIdentityPoolId = Bundle.main.infoDictionary!["AWSIdentityPoolId"] as? String
+    static let awsRegion = Bundle.main.infoDictionary!["AWSRegion"] as? String
+    static let awsSNSTopic = Bundle.main.infoDictionary!["AWSSNSTopic"] as? String
+    
+    static let wordOfTheDay: T = (wordOfTheDayPath!, { return Mapper<Word>().map(JSONObject: $0) })
+
+}
